caaa602656ffdc40e819b1fb546dd0ad2fd1a4fb
Lucas Manzano <lucasmfarias1@gmail.com>
2018-04-29 20:46:29 -0300

    Set up Devise and Action Mailer

--- Gemfile
+++ Gemfile
@@ -50,5 +50,11 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+# Basic authentication
+gem 'devise'
+
+# env variables
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,17 @@ module Soloq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       :plain,
+    enable_starttls_auto: true
+    }
   end
 end
